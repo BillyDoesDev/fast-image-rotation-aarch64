5ff22ab63a5bbbcd36abb9366b565516687061d9
BillyDoesDev <64155209+BillyDoesDev@users.noreply.github.com>
2024-06-24 12:56:51 +0530

    so turns out, keeping an unknown x for the starting line eq works

--- utils/dlr_test_zone1.py
+++ utils/dlr_test_zone1.py
@@ -9,31 +9,31 @@ except FileNotFoundError:
 
 m, n = img.shape[:2]
 
-angle = 89
+angle = 10
 alpha = (pi * angle) / 180
 
-mrt = m + n                                         # new image dimensions
-nrt = mrt                                           # new image dimensions                                       
+mrt = m + n                             # new image dimensions
+nrt = mrt                               # new image dimensions
 x_offset = ceil(m * sin(alpha))
-# fs = lambda x: tan(alpha) * x                       # starting line equation
-fs = lambda y: y / tan(alpha)                       # starting line equation
+fs = lambda y: y / tan(alpha + pi / 2)  # starting line equation
 
 rot = np.zeros((mrt, nrt), dtype=img.dtype)
 
-delta_x = sin(alpha)
-delta_y = cos(alpha)
+delta_x = cos(alpha)
+delta_y = sin(alpha)
 
-for i in range(ceil(n * sin(alpha))):
+for i in range(ceil(m * cos(alpha))):
     x_, y_ = fs(i), i
-    
-    for px in range(m):
+
+    for px in range(n):
         try:
             x, y = round(x_), round(y_)
-            rot[y, x + x_offset] = img[px, i]
-            x_ -= delta_x
+            rot[y, x + x_offset] = img[i, px]
+            rot[y + 1, x + x_offset] = img[i, px]
+            x_ += delta_x
             y_ += delta_y
         except IndexError:
-            print(f"[index err] mapping {(x + x_offset, y)} to {(i, px)}")
+            print(f"[index err] mapping {(x + x_offset, y)} to {(px, i)}")
             pass
 
 
