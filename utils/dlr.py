9f163dd649d157b61d91466ddcba8de308778f2b
BillyDoesDev <64155209+BillyDoesDev@users.noreply.github.com>
2024-06-30 03:57:29 +0530

    fully implemened dlr

---
+++ utils/dlr.py
@@ -0,0 +1,121 @@
+import matplotlib.pyplot as plt
+from math import *
+import numpy as np
+
+def rotate(img, rot, x_offset, y_offset, delta_x, delta_y, delta_i, fs, outer_limit, last_px, horizontal, negate_coords=False):
+    print("rotating...")
+    src_i = 0
+    shift = 1
+    for i in range(outer_limit):
+        x_, y_ = fs(i), i
+        for px in range(last_px):
+            try:
+                x, y, i_ = [floor(_) for _ in (x_, y_, src_i)]
+                if negate_coords:
+                    shift = -1
+                
+                a, b = (px, i_) if not horizontal else (i_, px)
+
+                rot[shift * (y + y_offset), shift * (x + x_offset)] = img[a, b] # y, x
+                rot[shift * (y + y_offset) + 1, shift * (x + x_offset)] = img[a + shift, b + shift]
+                x_ += delta_x
+                y_ += delta_y
+            except IndexError:
+                # print("[index err]")
+                pass
+
+        src_i += delta_i
+
+
+def main():
+    try:
+        img = plt.imread("./assets/fish.png")
+    except FileNotFoundError:
+        img = plt.imread("../assets/fish.png")
+
+    m, n = img.shape[:2]
+
+    angle = 360
+    alpha = (pi * angle) / 180
+
+    sin_alpha = abs(sin(alpha))
+    cos_alpha = abs(cos(alpha))
+    tan_alpha = tan(alpha)
+    tan_alpha_ = tan(alpha + pi / 2)
+
+    mrt = ceil(m * cos_alpha + n * sin_alpha)
+    nrt = ceil(m * sin_alpha + n * cos_alpha)
+    rot = np.zeros((mrt, nrt), dtype=img.dtype)
+
+    print(f"{(m, n, mrt, nrt) = }")
+
+    # zone 2 and 6
+    if (z2 := 45 < angle <= 90) or (225 < angle <= 270):
+        x_offset = ceil(m * sin_alpha)
+        y_offset = 0
+        
+        fs = lambda y: y / tan_alpha                  # starting line equation
+        delta_x = -sin_alpha
+        delta_y = cos_alpha
+        delta_i = (-1 if z2 else 1) / delta_x
+
+        outer_limit = ceil(n * sin_alpha)
+        last_px = m
+        rotate(img, rot, x_offset, y_offset, delta_x, delta_y, delta_i, fs, outer_limit, last_px, horizontal=False)
+    
+    # zone 3 and 7
+    elif (z2 := 90 < angle <= 135) or (270 < angle <= 315):
+        x_offset = nrt
+        y_offset = ceil(m * cos_alpha)
+        
+        fs = lambda y: y / tan_alpha                       # starting line equation
+        delta_x = -sin_alpha
+        delta_y = -cos_alpha
+        delta_i = (-1 if z2 else 1) / delta_x
+
+        outer_limit = ceil(n * sin_alpha)
+        last_px = m
+        rotate(img, rot, x_offset, y_offset, delta_x, delta_y, delta_i, fs, outer_limit, last_px, horizontal=False)
+    
+    # zone 1 and 5
+    elif (z1 := 0 < angle <= 45) or (180 < angle <= 225):
+        x_offset = ceil(m * sin_alpha)
+        y_offset = 0
+        
+        fs = lambda y: y / tan_alpha_  # starting line equation
+        delta_x = cos_alpha
+        delta_y = sin_alpha
+        delta_i = 1 / delta_x
+
+        outer_limit = ceil(m * cos_alpha)
+        last_px = n
+        rotate(img, rot, x_offset, y_offset, delta_x, delta_y, delta_i, fs, outer_limit, last_px, horizontal=True, negate_coords=not z1)
+    
+    # zone 4 and 8
+    elif (z1 := 135 < angle <= 180) or (135 < angle <= 360):
+        x_offset = ceil(n * cos_alpha)
+        y_offset = 0
+        
+        fs = lambda y: y / tan_alpha_  # starting line equation
+        delta_x = -cos_alpha
+        delta_y = sin_alpha
+        delta_i = 1 / delta_x
+
+        outer_limit = ceil(m * cos_alpha)
+        last_px = n
+        rotate(img, rot, x_offset, y_offset, delta_x, delta_y, delta_i, fs, outer_limit, last_px, horizontal=True, negate_coords=not z1)
+
+
+    plt.figure(figsize=(10, 5))
+    plt.subplot(1, 2, 1)
+    plt.title("Original Image")
+    plt.imshow(img, cmap="gray")
+
+    plt.subplot(1, 2, 2)
+    plt.title("Rotated Image")
+    plt.imshow(rot, cmap="gray")
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
